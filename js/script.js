3e3029a012eb19960a743b6d4cc29e0c59bc8165
Mohammad <mghasemi@mail.ru>
2021-03-25 14:17:18 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//alert('you have failed this city');
+
+
+//const result = confirm ('Is Putin a killer?');
+//console.log(result);
+
+//const answer = prompt('who has failed this city?', 'you');
+//console.log(answer);
+
+const answer = prompt('What is your name?', ' ');
+const answer2 = prompt('What is your syrname?', ' ');
+alert(`Hello, ${answer} ${answer2}`);
+
